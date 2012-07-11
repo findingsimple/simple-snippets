5fcfadb894ca171c3e7dd2f73aa2e7a811ecfe26
Brent Shepherd <thenbrent@gmail.com>
2012-07-11 16:07:43 +1000

    Moving help into core snippets class.

--- simple-snippets.php
+++ simple-snippets.php
@@ -46,12 +46,16 @@ class Simple_Snippets {
 
 	static $post_type_name;
 
+	static $admin_screen_id;
+
 	public static function init() {
 		global $wp_version;
 
 		self::$text_domain = apply_filters( 'simple_snippets_text_domain', 'Simple_Snippets' );
 
 		self::$post_type_name = apply_filters( 'simple_snippets_post_type_name', 'snippet' );
+
+		self::$admin_screen_id = apply_filters( 'simple_snippets_admin_screen_id', 'snippet' );
 
 		add_action( 'init', array( __CLASS__, 'register_post_type' ) );
 
@@ -69,10 +73,9 @@ class Simple_Snippets {
 
 		add_action( 'save_post', array( __CLASS__, 'save_snippet_meta' ) );
 
-		if ( is_admin() ) {
-			require_once( 'classes/help.php' );
-			Simple_Snippets_Help::init();
-		}
+		// In context help 
+		add_action( 'load-post.php', array( __CLASS__, 'add_help_tabs' ) );
+		add_action( 'load-post-new.php', array( __CLASS__, 'add_help_tabs' ) );
 	}
 
 	/**
@@ -497,6 +500,84 @@ QTags.addButton('post_snippets_id', 'snippet', function() {
 		return content_url( $post_content_path . $file );
 	}
 
+	/* Help tabs */
+
+	/**
+	 * Setup the help tabs and sidebar.
+	 *
+	 * @since 1.0
+	 */
+	public static function add_help_tabs() {
+
+		$screen = get_current_screen();
+
+		if ( $screen->id == self::$admin_screen_id ) {
+
+			$screen->add_help_tab( array(
+				'id'      => 'basic-plugin-help',
+				'title'   => __( 'Basic', Simple_Snippets::$text_domain ),
+				'content' => self::help_tab_basic()
+			) );
+
+			$screen->add_help_tab( array(
+				'id'      => 'variables-plugin-help',
+				'title'   => __( 'Variables', Simple_Snippets::$text_domain ),
+				'content' => self::help_tab_variables()
+			) );
+		}
+	}
+
+	/**
+	 * The basic help tab.
+	 * 
+	 * @since 1.0
+	 * @return	string	The help text
+	 */
+	public static function help_tab_basic() {
+		ob_start(); ?>
+<h2><?php _e( 'Title', Simple_Snippets::$text_domain ); ?></h2>
+<p><?php _e( 'The snippet title is used to identify the snippet. This will also become the name of the shortcode if you enable that option.', Simple_Snippets::$text_domain ); ?></p>
+
+<h2><?php _e( 'Content', Simple_Snippets::$text_domain ); ?></h2>
+<p><?php _e( 'Create HTML content for your snippet just as you would create content for a post or page. You can use shortcodes and even other snippets within your snippet\'s content. To use variables in the content, reference them between curly braces e.g. <code>{variable_name}</code>.', Simple_Snippets::$text_domain ); ?></p>
+
+<h2><?php _e( 'Description', Simple_Snippets::$text_domain ); ?></h2>
+<p><?php _e( 'Include an optional explanation or description of the snippet. If filled out, the description will be displayed in insert snippet window of the post editor.', Simple_Snippets::$text_domain); ?></p>
+
+<h2><?php _e( 'Shortcode', Simple_Snippets::$text_domain ); ?></h2>
+<p><?php _e( 'When enabling the shortcode checkbox, the snippet is no longer inserted into a post as HTML, instead it is inserted as a shortcode. The advantage of a shortcode is that you can insert a block of text or code in many places on the site, and update the content from one single place.', Simple_Snippets::$text_domain ); ?></p>
+<p><?php _e( 'The name to use the shortcode is the same as the title of the snippet (with spaces replaced by hypehens). When inserting a snippet as a shortcode, the shortcode tag will be inserted into the post instead of the HTML content i.e. [snippet-name].', Simple_Snippets::$text_domain ); ?></p>
+
+<?php 
+		return ob_get_clean();
+	}
+
+	/**
+	 * The basic help tab.
+	 * 
+	 * @since 1.0
+	 * @return	string	The help text
+	 */
+	public static function help_tab_variables() {
+		ob_start(); ?>
+<h2><?php _e( 'Variables', Simple_Snippets::$text_domain ); ?></h2>
+<p><?php _e( 'You can use variables to dynamically change certain values in your snippet. A variable can also be assigned a default value.', Simple_Snippets::$text_domain ); ?></p>
+
+<h3><?php _e( 'Variable Names', Simple_Snippets::$text_domain ); ?></h3>
+<p><?php _e( 'Variable names should be unique. For best results, a variable name should only contain letters (a-z), numbers (0-9) and hyphens.', Simple_Snippets::$text_domain ); ?></p>
+
+<h3><?php _e( 'Variable Values', Simple_Snippets::$text_domain ); ?></h3>
+<p><?php _e( 'A variable can be assigned a default value which will be used if no other value is provided.', Simple_Snippets::$text_domain ); ?></p>
+
+<h3><?php _e( 'Using a Variable', Simple_Snippets::$text_domain ); ?></h3>
+<p><?php _e( 'To use a variable in a snippet, insert the variable name enclosed in curly braces. For example, to use a variable named <code>var_one</code>, add <code>{var_one}</code> to your snippet.', Simple_Snippets::$text_domain ); ?></p>
+
+<h3><?php _e( 'Variable Select Box', Simple_Snippets::$text_domain ); ?></h3>
+<p><?php _e( 'To constrain the available values for a variable to a specific list of items, insert a comma separated list of values enclosed in curly braces in the <em>Default Value/s</em> field.', Simple_Snippets::$text_domain ); ?></p>
+<p><?php _e( 'For example, entering <code>{ACT,NSW,NT,QLD,SA,TAS,VIC,WA}</code> in the <em>Default Value/s</em> field would display a select box with States and Territories when inserting a snippet.', Simple_Snippets::$text_domain ); ?></p>
+	<?php 
+		return ob_get_clean();
+	}
 }
 
 Simple_Snippets::init();
